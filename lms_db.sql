d992ae917a07c81f2912bf267ebb6eff7841522d
rushi <rushikeshsangale57@gmail.com>
2024-09-28 13:40:10 +0530

    User Detail table added to LMS database for Login purpose

--- lms_db.sql
+++ lms_db.sql
@@ -1,2 +1,13 @@
-create database lms;
+
+CREATE TABLE User_Details (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    contact_number VARCHAR(15) NOT NULL,
+    verified BOOLEAN DEFAULT FALSE,
+    creator_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    creator_user VARCHAR(100) NOT NULL
+);
 
